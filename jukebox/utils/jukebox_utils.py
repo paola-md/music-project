f0a03131361205cfd4d1bc37f9f60e4fca5c2618
Baran Ozaydin <Baran.ozaydin@epfl.ch>
2022-05-23 16:50:37 +0000

    Add jukebox encoder sequential breakdown

---
+++ jukebox/utils/jukebox_utils.py
@@ -0,0 +1,121 @@
+from functools import partial
+import functools
+from collections import OrderedDict
+
+
+import torch
+
+def nonlinearity(x):
+    # swish
+    return x*torch.sigmoid(x)
+
+def get_forward_calls_enc_block(block, prefix=""):
+    if prefix == "":
+        pass
+    else:
+        prefix = prefix + "-"
+    
+    calls = []
+    layer_names = []
+    
+    for i_block in range(len(block.model)):
+        calls.append(block.model[i_block])
+        layer_names.append(prefix + f"model-{i_block}")
+    
+    
+    return calls, layer_names
+
+def get_forward_calls_encoder(encoder, prefix=""):
+    if prefix == "":
+        pass
+    else:
+        prefix = prefix + "-"
+        
+    calls = []
+    layer_names = []
+    
+    iterator = zip(list(range(encoder.levels)), encoder.downs_t, encoder.strides_t)
+    for level, down_t, stride_t in iterator:
+        #calls.append(encoder.level_blocks[level])
+        #layer_names.append(prefix + f"level_blocks-{level}")
+        
+        calls_, layer_names_ = get_forward_calls_enc_block(encoder.level_blocks[level], prefix + f"level_blocks-{level}")
+        
+        calls += calls_
+        layer_names += layer_names_
+    
+    
+    return calls, layer_names
+
+def get_forward_calls_decoder(decoder, prefix=""):
+    if prefix == "":
+        pass
+    else:
+        prefix = prefix + "-"
+        
+    calls = []
+    layer_names = []
+    
+    iterator = reversed(list(zip(list(range(decoder.levels)), decoder.downs_t, decoder.strides_t)))
+    for level, down_t, stride_t in iterator:
+        #calls.append(encoder.level_blocks[level])
+        #layer_names.append(prefix + f"level_blocks-{level}")
+        
+        calls_, layer_names_ = get_forward_calls_enc_block(decoder.level_blocks[level], prefix + f"level_blocks-{level}")
+        
+        calls += calls_
+        layer_names += layer_names_
+    
+    
+    return calls, layer_names
+
+
+
+
+
+def compose2func(f, g):
+    return lambda *a, **kw: g(f(*a, **kw))
+
+def compose_funclist(fs):
+    if len(fs) == 0:
+        return lambda x: x
+    else:
+        return functools.reduce(compose2func, fs)
+    
+                                     
+def split_model(model, split_layer_name, get_calls, sequential=True):
+    calls, layer_names = get_calls(model)
+    assert split_layer_name in layer_names
+    i = layer_names.index(split_layer_name)
+    
+    pre_modules = calls[:i + 1]
+    pre_module_names = layer_names[:i + 1]
+    post_modules = calls[i + 1:]
+    post_module_names = layer_names[i + 1:]
+    
+    if sequential:
+        pre = torch.nn.Sequential(OrderedDict([(n, l) for n, l in zip(pre_module_names, pre_modules)]))
+        post = torch.nn.Sequential(OrderedDict([(n, l) for n, l in zip(post_module_names, post_modules)]))
+    else:
+        pre = compose_funclist(pre)
+        post = compose_funclist(post)
+    
+    return pre, post
+
+
+import torchvision
+from PIL import Image
+
+
+def preprocess_vqgan_input(img, target_image_size=256):
+    s = min(img.size)
+    
+    if s < target_image_size:
+        raise ValueError(f'min dim for image {s} < {target_image_size}')
+        
+    r = target_image_size / s
+    s = (round(r * img.size[1]), round(r * img.size[0]))
+    img = torchvision.transforms.functional.resize(img, s, interpolation=Image.LANCZOS)
+    img = torchvision.transforms.functional.center_crop(img, output_size=2 * [target_image_size])
+    img = torch.unsqueeze(torchvision.transforms.ToTensor()(img), 0)
+    return 2.0 * img - 1 # Normalize to range [-1, 1]
--- lucent/optvis/render.py
+++ lucent/optvis/render.py
@@ -304,7 +304,7 @@ def export(tensor, image_name=None):
         image = np.concatenate(image, axis=1)
     Image.fromarray(image).save(image_name)
 
-
+'''
 class ModuleHook:
     def __init__(self, module):
         self.hook = module.register_forward_hook(self.hook_fn)
@@ -347,4 +347,47 @@ def hook_model(model, image_f):
         return out
 
     return hook
+'''
+class ModuleHook:
+    def __init__(self, module):
+        self.hook = module.register_forward_hook(self.hook_fn)
+        self.module = None
+        self.features = None
+
+    def hook_fn(self, module, input, output):
+        self.module = module
+        self.features = output
+
+    def close(self):
+        self.hook.remove()
+
+
+def hook_model(model, include_class_name=True):
+    features = OrderedDict()
+    layers = OrderedDict()
+    # recursive hooking function
+    def hook_layers(net, prefix=[]):
+        if hasattr(net, "_modules"):
+            for name, layer in net._modules.items():
+                if layer is None:
+                    # e.g. GoogLeNet's aux1 and aux2 layers
+                    continue
+                hook_layers(layer, prefix=prefix + [name])
+                if include_class_name:
+                    features["-".join(prefix + [name]) + f":{layer.__class__.__name__}"] = ModuleHook(layer)
+                    layers["-".join(prefix+[name]) + f":{layer.__class__.__name__}"] = layer.__repr__()
+                else:
+                    features["-".join(prefix + [name])] = ModuleHook(layer)
+                    layers["-".join(prefix+[name])] = layer.__repr__()
+
+    hook_layers(model)
+
+    def hook(layer):
+        assert layer in features, f"Invalid layer {layer}. Retrieve the list of layers with `lucent.modelzoo.util.get_model_layers(model)`."
+        out = features[layer].features
+        assert out is not None, "There are no saved feature maps. Make sure to put the model in eval mode, like so: `model.to(device).eval()`. See README for example."
+        return out
+
+    return hook, layers
+
 
