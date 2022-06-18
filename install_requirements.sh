52d65757732a3ee4bb8f3523fed7aee2e4866af9
Paola Mejia <mejia@IC-ITs-MacBook-Pro-2.local>
2022-06-19 01:29:13 +0200

    clean notebooks

---
+++ install_requirements.sh
@@ -0,0 +1,12 @@
+pip3 install librosa
+sudo apt-get update
+sudo apt-get install -y libsndfile1
+sudo apt-get install -y sox
+pip3 install soundfile
+pip3 install fire
+sudo conda install -y mpi4py
+pip3 install kornia
+pip3 install torch_audiomentations
+pip3 install nussl
+pip3 install streamlit
+sudo conda install -c conda-forge ffmpeg -y
